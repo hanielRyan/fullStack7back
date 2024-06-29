a63cc511869c0218ad9ca62e36e76be9229244a7
Haniel Ryan <hanielryanephin@gmail.com>
2024-06-29 20:17:08 +0400

    add vercel

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage,
 })
-
+ 
 app.post("/uploadPic",upload.single("img"),async(req,res)=>{
     const email = req.body.email;
     const user = await userModel.findOne({email}).exec();
